a529e2e46dd4dd852ae463c65c6eafe201eb24a4
Adarsh Singh <singh06vikash@gmail.com>
2025-07-30 11:14:07 +0530

    captain registration done

--- Backend/app.js
+++ Backend/app.js
@@ -11,6 +11,9 @@ const connectToDb = require('./db/db');
 connectToDb();
 
 const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,5 +25,6 @@ app.get('/' , (req,res) => {
 });
 
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 module.exports = app;
